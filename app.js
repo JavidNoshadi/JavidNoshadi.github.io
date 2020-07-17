37d43ec2d3afbf32af395db6419029475cee78eb
JavidNoshadi <javid.king63@gmail.com>
2020-07-17 06:14:46 +0430

    6am

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ function colors() {
   let num2 = Math.floor(Math.random() * 255)
   let num3 = Math.floor(Math.random() * 255)
   return nums = `${num1},${num2},${num3}`
-   
 }
 colors();
+let avatar = document.querySelector('.avatar img')
+let footer = document.querySelector('.footer')
+avatar.style.border = `2px solid rgb(${nums})`;
+footer.style.borderTop = `2px solid rgb(${nums})`;
+
 console.log(nums);
