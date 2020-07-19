7bc8cc8ed6627600d2258ded755388841f2d134e
JavidNoshadi <javid.king63@gmail.com>
2020-07-20 00:53:20 +0430

    q

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+ /*setTimeout(function() {
+  let greeting = document.getElementById('greeting')
+  let name = prompt('Enter your name')
+  greeting.innerText = name
+  let visitor = greeting.innerText
+  return visitor
+}, 3000) */
+window.addEventListener('load', () => {
+  localStorage.getItem('body')
+ // localStorage.setItem('visitor', visitor)
+})
 let body = document.querySelector('body')
 // i use generated nums to get diffrent colors . so each time user visit the site, they will see diffrent colors as base theme:)
 function colors() {
@@ -13,7 +24,9 @@ avatar.style.border = `2px solid rgb(${nums})`;
 head.style.color = `rgb(${nums})`;
 let dot = document.querySelector('.dot')
 let loader = document.querySelector('.loader')
-dot.addEventListener('animationend',()=>{
+dot.addEventListener('animationend', () => {
   loader.style.display = 'none'
 })
 console.log(dot)
+localStorage.setItem('body', body)
+console.log(body);
