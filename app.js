5f8696a509d122b5ee7b82741ec853ea43a8c03c
JavidNoshadi <javid.king63@gmail.com>
2020-07-20 23:29:49 +0430

    cleared unused codes..

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
- /*setTimeout(function() {
-  let greeting = document.getElementById('greeting')
-  let name = prompt('Enter your name')
-  greeting.innerText = name
-  let visitor = greeting.innerText
-  return visitor
-}, 3000) */
-window.addEventListener('load', () => {
- // localStorage.setItem('visitor', visitor)
- console.log('loaded');
-})
-let body = document.querySelector('body')
-// i use generated nums to get diffrent colors . so each time user visit the site, they will see diffrent colors as base theme:)
-function colors() {
-  let num1 = Math.floor(Math.random() * 255)
-  let num2 = Math.floor(Math.random() * 255)
-  let num3 = Math.floor(Math.random() * 255)
-  return nums = `${num1},${num2},${num3}`
-}
-colors();
-let avatar = document.querySelector('.avatar img')
-let head = document.querySelector('.head h1')
-avatar.style.border = `2px solid rgb(${nums})`;
-head.style.color = `rgb(${nums})`;
-let loader = document.querySelector('.loader')
-let dot = document.querySelector('.dot')
+const body = document.querySelector('body')
+const loader = document.querySelector('.loader')
+const dot = document.querySelector('.dot')
 dot.addEventListener('animationend', () => {
   loader.style.display = 'none'
 })
-console.log(dot)
