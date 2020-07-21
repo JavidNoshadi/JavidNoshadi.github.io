8f1f87da3372bb02a48497dfedf662c89441ba29
JavidNoshadi <javid.king63@gmail.com>
2020-07-22 01:59:03 +0430

    still needs shits to do. but it looks damn beautiful now !

--- app.js
+++ app.js
@@ -4,17 +4,10 @@ const dot = document.querySelector('.dot')
 dot.addEventListener('animationend', () => {
   loader.style.display = 'none'
 })
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector('.feather-menu')
 const links = document.querySelector('.links')
 hamburger.addEventListener('click',()=>{
   links.classList.toggle('open')
+  hamburger.classList.toggle('active')
   console.log('toggled');
 })
-/*
- 
- const svgs = document.querySelectorAll('.waves path')
-svgs.forEach((svg)=>{
- let path = svg.getAttribute('d')
- svg.style.animation = 'switchpath 5s ease infinite'
-   console.log(path);
-}) */
