5d9f1979655fbe49c6cfadc9038765e90934e746
JavidNoshadi <javid.king63@gmail.com>
2020-07-22 14:40:57 +0430

    i

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const dot = document.querySelector('.dot')
 dot.addEventListener('animationend', () => {
   loader.style.display = 'none'
 })
-const hamburger = document.querySelector('.feather-menu')
+const hamburger = document.querySelector('.menu')
 const links = document.querySelector('.links')
+const lines = document.querySelectorAll('.ham')
 hamburger.addEventListener('click',()=>{
   links.classList.toggle('open')
+ // hamburger.setAttribute('stroke-fill','red')
   hamburger.classList.toggle('active')
+  lines.forEach((line)=>{
+      line.classList.toggle('active')
+
+  })
   console.log('toggled');
 })
